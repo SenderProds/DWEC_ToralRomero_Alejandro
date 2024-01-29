4c96e3fb4b32a69a5b60d3a7c4fa6911dd894533
SenderProds <alextoralromero04@gmail.com>
2024-01-29 10:52:35 +0100

    Ejercicio 2_2

--- unit4/task2_2/script.js
+++ unit4/task2_2/script.js
@@ -1,3 +1,5 @@
+
+
 for (const elemento of document.querySelectorAll("*")) {
     elemento.addEventListener("click", (evento) => {
         console.log("Bubbling phase: Estoy en "+evento.currentTarget.tagName+" y el evento lo lanzó "+evento.target.tagName);
@@ -5,5 +7,5 @@ for (const elemento of document.querySelectorAll("*")) {
 
     elemento.addEventListener("click", (evento) => {
         console.log("Capturing phase: Estoy en "+evento.currentTarget.tagName+" y el evento lo lanzó "+evento.target.tagName);
-    },{capture:true}); //Poner capture para luego no confundirse con otros (once para que se ejecute una vez)
+    },{capture:true}); 
 };
