81925202e1fa829714adf890c2d1c59c93be0e81
SenderProds <alextoralromero04@gmail.com>
2024-02-05 11:55:53 +0100

    Actividad2_3/2_4

--- unit4/task2_4ui_events/hipoteca.js
+++ unit4/task2_4ui_events/hipoteca.js
@@ -1,17 +1,4 @@
-/*\
-    n\ tform = document.myform;\
-    n\ tLoanAmount = form.LoanAmount.value;\
-    n\ tDownPayment = "0";\
-    n\ tAnnualInterestRate = form.InterestRate.value / 100;\
-    n\ tYears = form.NumberOfYears.value;\
-    n\ tMonthRate = AnnualInterestRate / 12;\
-    n\ tNumPayments = Years * 12;\
-    n\ tPrin = LoanAmount - DownPayment;\
-    n\ tMonthPayment = Math.floor((Prin * MonthRate) / (1 - Math.pow((1 + MonthRate), (-1 * NumPayments))) * 100) / 100;\
-    n\ tform.NumberOfPayments.value = NumPayments;\
-    n\ tform.MonthlyPayment.value = MonthPayment;\
-    n
-}*/
+
 
 let boton = document.getElementById('calcular');
 
