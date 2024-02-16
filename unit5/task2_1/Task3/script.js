2a7df72b8bfdf66303a09db44ff044f69cebce38
SenderProds <alextoralromero04@gmail.com>
2024-02-16 10:09:25 +0100

    Ejercicio 2_1

--- unit5/task2_1/Task3/script.js
+++ unit5/task2_1/Task3/script.js
@@ -1,11 +1,42 @@
-fetch('https://github.com/frontid/ComunidadesProvinciasPoblaciones/blob/master/provincias.json')
-.then((response) => {
-    if(!response.ok){
-        throw new Error('Error json');
+const url = "archivo.php";
+let array = [];
+
+fetch(url)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Error en el JSON");
     }
 
     return response.json();
-})
-.then((data) => {
-    console.log(data);
+  })
+  .then((data) => {
+    array = data;
+  });
+
+let inputCiudad = document.getElementById("ciudad");
+let seccionSugerencias = document.getElementById("sugerencias");
+
+inputCiudad.addEventListener("input", (e) => {
+  let ulSugerencias = document.createElement("ul");
+  let valor = e.target.value;
+
+  seccionSugerencias.innerHTML = " ";
+  if (valor != "") {
+    let ciudadesFiltradas = array.filter((name) =>
+      name.toLowerCase().startsWith(valor)
+    );
+
+    ciudadesFiltradas.forEach((ciudad) => {
+      let li = document.createElement("li");
+      li.textContent = ciudad;
+      li.addEventListener('click', (e) => {
+        inputCiudad.value = ciudad;
+      });
+      ulSugerencias.appendChild(li);
+    });
+
+    
+    seccionSugerencias.appendChild(ulSugerencias);
+    
+  }
 });
