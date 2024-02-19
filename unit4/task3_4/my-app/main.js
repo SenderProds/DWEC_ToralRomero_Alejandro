021c4a1b6d98e948ed40385efe7671cfef7aa9d8
SenderProds <alextoralromero04@gmail.com>
2024-02-19 19:00:05 +0100

    Actividad 5_4_1

--- unit4/task3_4/my-app/main.js
+++ unit4/task3_4/my-app/main.js
@@ -1,6 +1,7 @@
 import './style.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
+import Draw from 'ol/interaction/Draw.js';
 import OSM from 'ol/source/OSM';
 import Feature from 'ol/Feature';
 import Point from 'ol/geom/Point';
@@ -9,6 +10,7 @@ import {Vector as VectorLayer} from 'ol/layer';
 import {Vector as VectorSource} from 'ol/source';
 
 
+/*
 if(navigator.geolocation){
   const map = new Map({
     target: 'map',
@@ -56,5 +58,57 @@ if(navigator.geolocation){
 }else{
   alert('Su navegador no es compatible con la geolocalizacion');
 }
+*/
 
+
+
+const raster = new TileLayer({
+  source: new OSM(),
+});
+
+const source = new VectorSource({wrapX: false});
+
+const vector = new VectorLayer({
+  source: source,
+});
+
+const map = new Map({
+  layers: [raster, vector],
+  target: 'map',
+  view: new View({
+    center: [-11000000, 4600000],
+    zoom: 4,
+  }),
+});
+
+const typeSelect = document.getElementById('type');
+
+let draw; // global so we can remove it later
+function addInteraction() {
+  const value = typeSelect.value;
+  if (value !== 'None') {
+    draw = new Draw({
+      source: source,
+      type: typeSelect.value,
+    });
+    console.log(source);
+    console.log(draw);
+    console.log(map);
+    map.addInteraction(draw);
+  }
+}
+
+/**
+ * Handle change event.
+ */
+typeSelect.onchange = function () {
+  map.removeInteraction(draw);
+  addInteraction();
+};
+
+document.getElementById('undo').addEventListener('click', function () {
+  draw.removeLastPoint();
+});
+
+addInteraction();
 
